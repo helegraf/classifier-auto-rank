b0a08adfe041a3f8773858044f7fe6c82d3f7c86
Helena Graf <Graf.Helena@web.de>
2017-12-31 18:24:47 +0100

    Add calculation for missing meta feature DefautlAccuracy

---
+++ ranker/src/main/java/ranker/DefaultAccuracy.java
@@ -0,0 +1,39 @@
+package ranker;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openml.webapplication.fantail.dc.Characterizer;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.rules.ZeroR;
+import weka.core.Instances;
+
+public class DefaultAccuracy extends Characterizer {
+	
+	private String[] ids = {"DefaultAccuracy"};
+
+	@Override
+	public String[] getIDs() {
+		return ids;
+	}
+
+	@Override
+	public Map<String, Double> characterize(Instances instances) {
+		Classifier classifier = new ZeroR();
+		double result;
+		try {
+			classifier.buildClassifier(instances);
+			Evaluation eval = new Evaluation(instances);
+			eval.evaluateModel(classifier, instances);
+			result = eval.pctCorrect();
+		} catch (Exception e) {
+			result = 0;
+		}
+		HashMap<String,Double> results = new HashMap<String,Double>();
+		results.put(ids[0], result);
+		return results;
+	}
+
+}
--- ranker/src/main/java/ranker/Main.java
+++ ranker/src/main/java/ranker/Main.java
@@ -1,6 +1,12 @@
 package ranker;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import org.openml.apiconnector.io.OpenmlConnector;
+import org.openml.webapplication.fantail.dc.Characterizer;
+import org.openml.webapplication.fantail.dc.statistical.Cardinality;
+import org.openml.webapplication.features.GlobalMetafeatures;
 
 import de.upb.cs.is.jpl.api.algorithm.learningalgorithm.labelranking.instancebasedlabelranking.InstanceBasedLabelRankingLearningAlgorithm;
 import de.upb.cs.is.jpl.api.algorithm.learningalgorithm.labelranking.instancebasedlabelranking.InstanceBasedLabelRankingLearningModel;
@@ -10,23 +16,30 @@ import de.upb.cs.is.jpl.api.dataset.labelranking.LabelRankingDataset;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-//		OpenmlConnector client = new OpenmlConnector();
-//		String[] dataQualities = client.dataQualitiesList().getQualities();
-//		System.out.println(dataQualities.length);
-//		
-//		DefaultDataset<IVector> dataset = new DefaultAbsoluteDataset();
-//		IVector ratings = new DenseDoubleVector(2, 5.0);
-//		dataset.addInstance(new DefaultInstance<IVector>(2, ratings, dataset));
-//		IInstance<?, ?, ?> instance = dataset.getInstance(0);
-//		Instance instance = Util.getQualities(5);
-//		ArrayList<Attribute> attributes = new ArrayList<Attribute>();
-//		for (int i = 0; i < Util.dataQualities.length; i++) {
-//			attributes.add(new Attribute(Util.dataQualities[i]));
-//		}
-//		attributes.add(new Attribute("Performance"));
-//		Instances results = new Instances("PerformanceMeasures", attributes, 0);
-//		// TODO set prediction
-//		results.add(instance);
+		GlobalMetafeatures features = new GlobalMetafeatures(null);
+		List<Characterizer> chars = features.getCharacterizers();
+		chars.add(new Cardinality());
+		chars.add(new DefaultAccuracy());
+		List<String> qual = new ArrayList<String>();
+		for (Characterizer ch : chars) {
+			String [] cra = ch.getIDs();
+			for (String str : cra) {
+				qual.add(str);
+			}
+		}
+		System.out.println(qual.size());
+		
+		OpenmlConnector client = new OpenmlConnector();
+		String []  realQual = client.dataQualitiesList().getQualities();
+		System.out.println(realQual.length);
+		
+		for (String str : realQual) {
+			if (qual.contains(str)) {
+				System.out.println("Contains " + str);
+			} else {
+				System.out.println("Does not contain " + str);
+			}
+		}
 		
 		
 //		Util.getAllDataQualities();
@@ -38,51 +51,45 @@ public class Main {
 //		List<Classifier> ranking = ranker.rank(Util.testSet.firstInstance());
 //		ranking.forEach(classifier->System.out.println(classifier.getClass().getName()));
 		
-		ArrayList<Integer> labels = new ArrayList<Integer>();
-		labels.add(0);
-		labels.add(1);
-		ArrayList<double[]> features = new ArrayList<double[]>();
-		double [] r1 = {1,0};
-		double [] r2 = {0,1};
-		double [] r3 = {1,1};
-		features.add(r1);
-		features.add(r2);
-		features.add(r3);
-		ArrayList<Ranking> rankings = new ArrayList<Ranking>();
-		int [] objectList = {0,1};
-		int [] compareOperators = {Ranking.COMPARABLE_ENCODING};
-		Ranking ranking = new Ranking(objectList, compareOperators);
-		rankings.add(ranking);
-		int [] objectList1 = {0,1};
-		int [] compareOperators1 = {Ranking.COMPARABLE_ENCODING};
-		Ranking ranking1 = new Ranking(objectList1, compareOperators1);
-		rankings.add(ranking1);
-		int [] objectList2 = {1,0};
-		int [] compareOperators2 = {Ranking.COMPARABLE_ENCODING};
-		Ranking ranking2 = new Ranking(objectList2, compareOperators2);
-		rankings.add(ranking2);
-		LabelRankingDataset test = new LabelRankingDataset(labels, features, rankings);
-		InstanceBasedLabelRankingLearningAlgorithm algo = new InstanceBasedLabelRankingLearningAlgorithm();
-		
-		InstanceBasedLabelRankingLearningModel model = algo.train(test);
-		
-		ArrayList<Integer> labels1 = new ArrayList<Integer>();
-		labels1.add(1);
-		labels1.add(0);
-		ArrayList<double[]> features1 = new ArrayList<double[]>();
-		double [] r4 = {0,0};
-		features1.add(r4);
-		ArrayList<Ranking> rankings2 = new ArrayList<Ranking>();
-		rankings2.add(null);
-		LabelRankingDataset train = new LabelRankingDataset(labels1, features1, rankings2);
-		
-		model.predict(train).forEach(item->System.out.println(item));
-		
-//		LabelRankingInstance arg0 = new LabelRankingInstance();
-//		arg0.setContextFeatureVector(r4);
-//		arg0.setRating(ranking21);
+//		ArrayList<Integer> labels = new ArrayList<Integer>();
+//		labels.add(0);
+//		labels.add(1);
+//		ArrayList<double[]> features = new ArrayList<double[]>();
+//		double [] r1 = {1,0};
+//		double [] r2 = {0,1};
+//		double [] r3 = {1,1};
+//		features.add(r1);
+//		features.add(r2);
+//		features.add(r3);
+//		ArrayList<Ranking> rankings = new ArrayList<Ranking>();
+//		int [] objectList = {0,1};
+//		int [] compareOperators = {Ranking.COMPARABLE_ENCODING};
+//		Ranking ranking = new Ranking(objectList, compareOperators);
+//		rankings.add(ranking);
+//		int [] objectList1 = {0,1};
+//		int [] compareOperators1 = {Ranking.COMPARABLE_ENCODING};
+//		Ranking ranking1 = new Ranking(objectList1, compareOperators1);
+//		rankings.add(ranking1);
+//		int [] objectList2 = {1,0};
+//		int [] compareOperators2 = {Ranking.COMPARABLE_ENCODING};
+//		Ranking ranking2 = new Ranking(objectList2, compareOperators2);
+//		rankings.add(ranking2);
+//		LabelRankingDataset test = new LabelRankingDataset(labels, features, rankings);
+//		InstanceBasedLabelRankingLearningAlgorithm algo = new InstanceBasedLabelRankingLearningAlgorithm();
 //		
-//		model.predict(arg0);
+//		InstanceBasedLabelRankingLearningModel model = algo.train(test);
+//		
+//		ArrayList<Integer> labels1 = new ArrayList<Integer>();
+//		labels1.add(1);
+//		labels1.add(0);
+//		ArrayList<double[]> features1 = new ArrayList<double[]>();
+//		double [] r4 = {0,0};
+//		features1.add(r4);
+//		ArrayList<Ranking> rankings2 = new ArrayList<Ranking>();
+//		rankings2.add(null);
+//		LabelRankingDataset train = new LabelRankingDataset(labels1, features1, rankings2);
+//		
+//		model.predict(train).forEach(item->System.out.println(item));
 		
 	}
 
