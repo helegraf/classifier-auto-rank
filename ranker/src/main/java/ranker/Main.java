c4a9c7f9b277ce59695211d90e16ac16a9cd232f
Helena Graf <Graf.Helena@web.de>
2018-01-05 01:11:40 +0100

    Misc changes to compute meta features

--- ranker/src/main/java/ranker/Main.java
+++ ranker/src/main/java/ranker/Main.java
@@ -1,96 +1,32 @@
 package ranker;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
-import org.openml.apiconnector.io.OpenmlConnector;
-import org.openml.webapplication.fantail.dc.Characterizer;
-import org.openml.webapplication.fantail.dc.statistical.Cardinality;
-import org.openml.webapplication.features.GlobalMetafeatures;
-
-import de.upb.cs.is.jpl.api.algorithm.learningalgorithm.labelranking.instancebasedlabelranking.InstanceBasedLabelRankingLearningAlgorithm;
-import de.upb.cs.is.jpl.api.algorithm.learningalgorithm.labelranking.instancebasedlabelranking.InstanceBasedLabelRankingLearningModel;
-import de.upb.cs.is.jpl.api.dataset.defaultdataset.relative.Ranking;
-import de.upb.cs.is.jpl.api.dataset.labelranking.LabelRankingDataset;
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		GlobalMetafeatures features = new GlobalMetafeatures(null);
-		List<Characterizer> chars = features.getCharacterizers();
-		chars.add(new Cardinality());
-		chars.add(new DefaultAccuracy());
-		List<String> qual = new ArrayList<String>();
-		for (Characterizer ch : chars) {
-			String [] cra = ch.getIDs();
-			for (String str : cra) {
-				qual.add(str);
-			}
-		}
-		System.out.println(qual.size());
-		
-		OpenmlConnector client = new OpenmlConnector();
-		String []  realQual = client.dataQualitiesList().getQualities();
-		System.out.println(realQual.length);
-		
-		for (String str : realQual) {
-			if (qual.contains(str)) {
-				System.out.println("Contains " + str);
-			} else {
-				System.out.println("Does not contain " + str);
-			}
-		}
-		
-		
-//		Util.getAllDataQualities();
-//		ArrayList<Integer> holdout = new ArrayList<Integer>();
-//		holdout.add(7);
-//		Util.makeInstances(holdout);
-//		Ranker ranker = new Ranker(RankingMode.REGRESSION);
-//		ranker.buildRanker(Util.classifierPerformances);
-//		List<Classifier> ranking = ranker.rank(Util.testSet.firstInstance());
-//		ranking.forEach(classifier->System.out.println(classifier.getClass().getName()));
-		
-//		ArrayList<Integer> labels = new ArrayList<Integer>();
-//		labels.add(0);
-//		labels.add(1);
-//		ArrayList<double[]> features = new ArrayList<double[]>();
-//		double [] r1 = {1,0};
-//		double [] r2 = {0,1};
-//		double [] r3 = {1,1};
-//		features.add(r1);
-//		features.add(r2);
-//		features.add(r3);
-//		ArrayList<Ranking> rankings = new ArrayList<Ranking>();
-//		int [] objectList = {0,1};
-//		int [] compareOperators = {Ranking.COMPARABLE_ENCODING};
-//		Ranking ranking = new Ranking(objectList, compareOperators);
-//		rankings.add(ranking);
-//		int [] objectList1 = {0,1};
-//		int [] compareOperators1 = {Ranking.COMPARABLE_ENCODING};
-//		Ranking ranking1 = new Ranking(objectList1, compareOperators1);
-//		rankings.add(ranking1);
-//		int [] objectList2 = {1,0};
-//		int [] compareOperators2 = {Ranking.COMPARABLE_ENCODING};
-//		Ranking ranking2 = new Ranking(objectList2, compareOperators2);
-//		rankings.add(ranking2);
-//		LabelRankingDataset test = new LabelRankingDataset(labels, features, rankings);
-//		InstanceBasedLabelRankingLearningAlgorithm algo = new InstanceBasedLabelRankingLearningAlgorithm();
-//		
-//		InstanceBasedLabelRankingLearningModel model = algo.train(test);
-//		
-//		ArrayList<Integer> labels1 = new ArrayList<Integer>();
-//		labels1.add(1);
-//		labels1.add(0);
-//		ArrayList<double[]> features1 = new ArrayList<double[]>();
-//		double [] r4 = {0,0};
-//		features1.add(r4);
-//		ArrayList<Ranking> rankings2 = new ArrayList<Ranking>();
-//		rankings2.add(null);
-//		LabelRankingDataset train = new LabelRankingDataset(labels1, features1, rankings2);
-//		
-//		model.predict(train).forEach(item->System.out.println(item));
-		
-	}
 
+//		BufferedReader reader = Files.newBufferedReader(Util.resultsPath.resolve("Perf").resolve("metafeatures.arff"),
+//				Util.charset);
+//		Instances instances = new Instances(reader);
+//		RandomForestRanker ranker = new RandomForestRanker();
+//		Util.testRanker(ranker, instances);
+		
+		List<Integer> datasets = Util.getDataSetsFromIndex();
+		Instances instances = Util.computeMetaFeatures(datasets);
+		ArffSaver saver = new ArffSaver();
+		saver.setInstances(instances);
+		try {
+			saver.setFile(new File(instances.relationName()));
+			saver.writeBatch();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- ranker/src/main/java/ranker/Util.java
+++ ranker/src/main/java/ranker/Util.java
@@ -20,10 +20,20 @@ import org.openml.apiconnector.xml.Data;
 import org.openml.apiconnector.xml.Data.DataSet;
 import org.openml.apiconnector.xml.DataFeature;
 import org.openml.apiconnector.xml.DataFeature.Feature;
+
+import de.upb.cs.is.jpl.api.algorithm.learningalgorithm.labelranking.instancebasedlabelranking.InstanceBasedLabelRankingLearningAlgorithm;
+import de.upb.cs.is.jpl.api.algorithm.learningalgorithm.labelranking.instancebasedlabelranking.InstanceBasedLabelRankingLearningModel;
+import de.upb.cs.is.jpl.api.algorithm.learningalgorithm.labelranking.labelrankingbypairwisecomparison.LabelRankingByPairwiseComparisonLearningAlgorithm;
+import de.upb.cs.is.jpl.api.algorithm.learningalgorithm.labelranking.labelrankingbypairwisecomparison.LabelRankingByPairwiseComparisonLearningModel;
+import de.upb.cs.is.jpl.api.dataset.defaultdataset.relative.Ranking;
+import de.upb.cs.is.jpl.api.dataset.labelranking.LabelRankingDataset;
+import de.upb.cs.is.jpl.api.exception.algorithm.PredictionFailedException;
+import de.upb.cs.is.jpl.api.exception.algorithm.TrainModelsFailedException;
+import rankerEvaluation.KendallRankCorrelation;
+import rankerEvaluation.LeaveOneOut;
+import rankerEvaluation.RankerEstimationProcedure;
+
 import org.openml.apiconnector.xml.DataSetDescription;
-import org.openml.webapplication.fantail.dc.Characterizer;
-import org.openml.webapplication.fantail.dc.statistical.Cardinality;
-import org.openml.webapplication.features.GlobalMetafeatures;
 
 import weka.classifiers.AbstractClassifier;
 import weka.classifiers.Classifier;
@@ -93,150 +103,210 @@ public class Util {
 	public static Map<Integer, double[]> dataQualitiesInstances = new HashMap<Integer, double[]>();
 	public static Map<Classifier, Instances> classifierPerformances = new HashMap<Classifier, Instances>();
 	public static Instances testSet;
-
-	public static double calculateKendallRankCorrelation(Classifier[] predictedOrdering, Classifier[] actualOrdering) {
-		// Convert Ordering given by Ranker into ranking
-		double[] xArray = new double[portfolio.length];
-		double[] yArray = new double[portfolio.length];
-
-		for (int i = 0; i < portfolio.length; i++) {
-			yArray[i] = i;
-			for (int j = 0; j < portfolio.length; j++) {
-				if (predictedOrdering[i].getClass().getName().equals(actualOrdering[j].getClass().getName())) {
-					xArray[i] = j;
-				}
-			}
-		}
-
-		KendallsCorrelation correlation = new KendallsCorrelation();
-
-		double result = correlation.correlation(xArray, yArray);
+	
+	public static double testRanker (Ranker ranker, Instances instances) {
+		// RegressionRanker ranker = new RegressionRanker();
+		// ranker.buildRanker(instances);
+		// ranker.predictRankingforInstance(instances.get(0)).forEach(action->System.out.println(action.getClass().getName()));
+		RankerEstimationProcedure estim = new LeaveOneOut();
+		double result = estim.estimate(ranker, new KendallRankCorrelation(), instances);
 		return result;
 	}
 
-	public static void makeInstances(List<Integer> holdout) throws IOException {
-		// aggregate for each classifier + add performance values
-		// make new dataset
-		ArrayList<Attribute> attributes = new ArrayList<Attribute>();
-		for (int i = 0; i < Util.dataQualities.length; i++) {
-			attributes.add(new Attribute(Util.dataQualities[i]));
-		}
-		attributes.add(new Attribute("Performance"));
-		Instances testset = new Instances("Testset", attributes, 0);
-		for (Classifier classifier : portfolio) {
-			Instances dataset = new Instances(classifier.getClass().getName(), attributes, 0);
-			dataset.setClassIndex(attributes.size() - 1);
-			// for each dataset : if result exists, add corresponding instance + performance
-			// to dataset
-			try (Stream<Path> paths = Files.walk(resultsPath)) {
-				paths.filter(Files::isRegularFile)
-						.filter(path -> path.getFileName().toString().contains(classifier.getClass().getName()))
-						.forEach(path -> {
-							try {
-								BufferedReader reader = Files.newBufferedReader(path);
-								String line = reader.readLine();
-								if (line != null) {
-									String element = path.getFileName().toString().split("_")[1];
-									String toAdd = element.substring(0, element.length() - 4);
-									int did = Integer.parseInt(toAdd);
-									if (!holdout.contains(did)) {
-										double result = Double.parseDouble(line);
-										// copy instances
-										double[] instanceValues = dataQualitiesInstances.get(did);
-										instanceValues[instanceValues.length - 1] = result;
-										Instance instance = new DenseInstance(dataQualities.length + 1, instanceValues);
-										dataset.add(instance);
-									}
-								}
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
+	public static void labelRankingTest() throws TrainModelsFailedException, PredictionFailedException {
+		ArrayList<Integer> labels = new ArrayList<Integer>();
+		labels.add(0);
+		labels.add(1);
+		ArrayList<double[]> features = new ArrayList<double[]>();
+		double[] r1 = { 1, 0 };
+		double[] r2 = { 0, 1 };
+		double[] r3 = { 1, 1 };
+		features.add(r1);
+		features.add(r2);
+		features.add(r3);
+		ArrayList<Ranking> rankings = new ArrayList<Ranking>();
+		int[] objectList = { 0, 1 };
+		int[] compareOperators = { Ranking.COMPARABLE_ENCODING };
+		Ranking ranking = new Ranking(objectList, compareOperators);
+		rankings.add(ranking);
+		int[] objectList1 = { 0, 1 };
+		int[] compareOperators1 = { Ranking.COMPARABLE_ENCODING };
+		Ranking ranking1 = new Ranking(objectList1, compareOperators1);
+		rankings.add(ranking1);
+		int[] objectList2 = { 0, 1 };
+		int[] compareOperators2 = { Ranking.COMPARABLE_ENCODING };
+		Ranking ranking2 = new Ranking(objectList2, compareOperators2);
+		rankings.add(ranking2);
+		LabelRankingDataset trainSet = new LabelRankingDataset(labels, features, rankings);
 
-						});
-			}
-			classifierPerformances.put(classifier, dataset);
-			ArffSaver saver = new ArffSaver();
-			saver.setInstances(dataset);
-			try {
-				saver.setFile(new File(
-						resultsPath.resolve("Perf").resolve(classifier.getClass().getName() + ".arff").toString()));
-				saver.writeBatch();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		InstanceBasedLabelRankingLearningAlgorithm algo = new InstanceBasedLabelRankingLearningAlgorithm();
+		InstanceBasedLabelRankingLearningModel model = algo.train(trainSet);
+		//LabelRankingByPairwiseComparisonLearningAlgorithm algo2 = new LabelRankingByPairwiseComparisonLearningAlgorithm();
+		//LabelRankingByPairwiseComparisonLearningModel model2 = algo2.train(trainSet);
+
+		ArrayList<Integer> labels1 = new ArrayList<Integer>();
+		labels1.add(1);
+		labels1.add(0);
+		ArrayList<double[]> features1 = new ArrayList<double[]>();
+		double[] r4 = { 0, 1 };
+		features1.add(r4);
+		ArrayList<Ranking> rankings1 = new ArrayList<Ranking>();
+		rankings1.add(null);
+		LabelRankingDataset testSet = new LabelRankingDataset(labels1, features1, rankings1);
+
+		model.predict(testSet).forEach(item -> System.out.println(item));
+		//model2.predict(testSet).forEach(item -> System.out.println(item));
+	}
+	
+	public static Instances computeMetaFeatures(List<Integer> dids) throws Exception {
+		// Prepare List of Attributes for Instances
+		ArrayList<Attribute> attributes = new ArrayList<Attribute>();
+
+		// Qualities
+		GlobalCharacterizer characterizer = new GlobalCharacterizer();
+		String[] allDataQualities = characterizer.getIDs();
+		Map<String, Integer> qualityIndices = new HashMap<String, Integer>();
+		for (int i = 0; i < allDataQualities.length; i++) {
+			String dataQuality = allDataQualities[i];
+			qualityIndices.put(dataQuality, i);
+			attributes.add(new Attribute(dataQuality));
 		}
-		for (int i : holdout) {
-			double[] instanceValues = dataQualitiesInstances.get(i);
-			Instance instance = new DenseInstance(dataQualities.length + 1, instanceValues);
-			testset.add(instance);
+
+		// Classifiers
+		Map<String, Integer> classifierIndices = new HashMap<String, Integer>();
+		for (int i = 0; i < portfolio.length; i++) {
+			String classifierName = portfolio[i].getClass().getName();
+			classifierIndices.put(classifierName, allDataQualities.length + i);
+			attributes.add(new Attribute(classifierName));
 		}
-		// save test set
-		ArffSaver saver = new ArffSaver();
-		testset.setClassIndex(testset.numAttributes() - 1);
-		saver.setInstances(testset);
-		try {
-			saver.setFile(new File(resultsPath.resolve("Perf").resolve("Test.arff").toString()));
-			saver.writeBatch();
-		} catch (IOException e) {
-			e.printStackTrace();
+
+		// Prepare instances
+		Instances instances = new Instances("MetaDataOpenML_calculated", attributes, 0);
+		HashMap<Integer, Instance> metaFeaturesForDataSets = new HashMap<Integer, Instance>();
+
+		// Add meta features
+		for (int dataSetId : dids) {
+			Instance instance = new DenseInstance(attributes.size());
+			Map<String, Double> dataQualities = characterizer.characterize(getInstancesById(dataSetId));
+			dataQualities.forEach((dataQuality, value) -> {
+				if (value != null) {
+					instance.setValue(qualityIndices.get(dataQuality), value);
+				}
+			});
+			metaFeaturesForDataSets.put(dataSetId, instance);
 		}
-		testSet = testset;
+
+		// Add performance results
+		try (Stream<Path> paths = Files.walk(resultsPath)) {
+			paths.filter(Files::isRegularFile).forEach(file -> {
+				try {
+					BufferedReader reader = Files.newBufferedReader(file, charset);
+					String line = reader.readLine();
+					if (line != null) {
+						String fileName = file.getFileName().toString();
+						fileName = fileName.substring(0, fileName.length() - 4);
+						String[] parts = fileName.split("_");
+						String classifierName = parts[0];
+						int dataSetId = Integer.parseInt(parts[1]);
+						if (dids.contains(dataSetId)) {
+							Instance instance = metaFeaturesForDataSets.get(dataSetId);
+							int attIndex = classifierIndices.get(classifierName);
+							double value = Double.parseDouble(line);
+							instance.setValue(attIndex, value);
+						}
+					}
+
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			});
+		}
+
+		instances.addAll(metaFeaturesForDataSets.values());
+		return instances;
 	}
 
-	public static void getAllDataQualities() throws Exception {
-		// read all datasets from .txt
+	public static Instances getMetaFeaturesFromOpenML() throws Exception {
+		// Prepare List of Attributes for Instances
+		ArrayList<Attribute> attributes = new ArrayList<Attribute>();
+
+		// Qualities
+		OpenmlConnector client = new OpenmlConnector();
+		String[] allDataQualities = client.dataQualitiesList().getQualities();
+		Map<String, Integer> qualityIndices = new HashMap<String, Integer>();
+		for (int i = 0; i < allDataQualities.length; i++) {
+			String dataQuality = allDataQualities[i];
+			qualityIndices.put(dataQuality, i);
+			attributes.add(new Attribute(dataQuality));
+		}
+
+		// Classifiers
+		Map<String, Integer> classifierIndices = new HashMap<String, Integer>();
+		for (int i = 0; i < portfolio.length; i++) {
+			String classifierName = portfolio[i].getClass().getName();
+			classifierIndices.put(classifierName, allDataQualities.length + i);
+			attributes.add(new Attribute(classifierName));
+		}
+
+		// Prepare instances
+		Instances instances = new Instances("MetaDataOpenML", attributes, 0);
+		HashMap<Integer, Instance> metaFeaturesForDataSets = new HashMap<Integer, Instance>();
+
+		// Add meta features
+		for (int dataSetId : getDataSetsFromIndex()) {
+			Instance instance = new DenseInstance(attributes.size());
+			Map<String, String> dataQualities = client.dataQualities(dataSetId).getQualitiesMap();
+			dataQualities.forEach((dataQuality, value) -> {
+				if (value != null) {
+					instance.setValue(qualityIndices.get(dataQuality), Double.parseDouble(value));
+				}
+			});
+			metaFeaturesForDataSets.put(dataSetId, instance);
+		}
+
+		// Add performance results
+		try (Stream<Path> paths = Files.walk(resultsPath)) {
+			paths.filter(Files::isRegularFile).forEach(file -> {
+				try {
+					BufferedReader reader = Files.newBufferedReader(file, charset);
+					String line = reader.readLine();
+					if (line != null) {
+						String fileName = file.getFileName().toString();
+						fileName = fileName.substring(0, fileName.length() - 4);
+						String[] parts = fileName.split("_");
+						String classifierName = parts[0];
+						int dataSetId = Integer.parseInt(parts[1]);
+						Instance instance = metaFeaturesForDataSets.get(dataSetId);
+						int attIndex = classifierIndices.get(classifierName);
+						double value = Double.parseDouble(line);
+						instance.setValue(attIndex, value);
+					}
+
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			});
+		}
+
+		instances.addAll(metaFeaturesForDataSets.values());
+		return instances;
+	}
+
+	public static List<Integer> getDataSetsFromIndex() throws Exception {
+		List<Integer> dataSets = new ArrayList<Integer>();
 		BufferedReader reader = Files.newBufferedReader(dataSetIndexPath, charset);
 		String line = null;
 		while ((line = reader.readLine()) != null) {
-			int did = Integer.parseInt(line);
-			dataQualitiesInstances.put(did, getQualities(did));
+			int dataSetId = Integer.parseInt(line);
+			dataSets.add(dataSetId);
 		}
-		// ad instance for each
-	}
-
-	public static double[] getQualities(int did) throws Exception {
-		OpenmlConnector client = new OpenmlConnector();
-		if (dataQualities == null) {
-			dataQualities = client.dataQualitiesList().getQualities();
-		}
-		double[] instance = new double[dataQualities.length + 1];
-
-		Map<String, String> qualities = client.dataQualities(did).getQualitiesMap();
-		for (int i = 0; i < dataQualities.length; i++) {
-			if (qualities.containsKey(dataQualities[i])) {
-				if (!(qualities.get(dataQualities[i]) == null)) {
-					instance[i] = Double.parseDouble(qualities.get(dataQualities[i]));
-				} else {
-					instance[i] = Double.NaN;
-				}
-
-			} else {
-				instance[i] = Double.NaN;
-			}
-		}
-
-		// calculate actual instance values
-		// save in .ARFF for each
-
-		GlobalMetafeatures allFeatures = new GlobalMetafeatures(null);
-		List<Characterizer> characterizers = allFeatures.getCharacterizers();
-		Cardinality cardinality = new Cardinality();
-		characterizers.add(cardinality);
-
-		Map<String, Double> results = new HashMap<String, Double>();
-		Instances dataset = getInstancesById(did);
-		for (Characterizer characterizer : characterizers) {
-			results.putAll(characterizer.characterize(dataset));
-		}
-
-		return instance;
-
+		return dataSets;
 	}
 
 	public static void generatePerformanceMeasures(List<Classifier> classifiers, List<Instances> datasets,
 			EvaluationMeasure evalM, EstimationProcedure estimProc, String filepath) {
-		// TODO refactor this method
 
 		// Prepare table of results
 		ArrayList<Attribute> attributes = new ArrayList<Attribute>();
@@ -269,16 +339,6 @@ public class Util {
 			e.printStackTrace();
 		}
 
-		// return "String";
-	}
-
-	public static void mergePerformanceMeasures() throws IOException {
-
-		// TODO decide how to parse sensibly
-		// read all generated Files
-		try (Stream<Path> paths = Files.walk(resultsPath)) {
-			paths.filter(Files::isRegularFile).forEach(System.out::println);
-		}
 	}
 
 	public static double generatePerformanceMeasure(Classifier classifier, Instances dataset) {
