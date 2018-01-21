57b9ca4337b1b8355e0fe1f7891af0e2aeaeebc7
GrafH <GrafH@DESKTOP-5QI48RS>
2018-01-21 19:30:51 +0100

    Add loss as evaluation measure

---
+++ ranker/src/main/java/rankerEvaluation/Loss.java
@@ -0,0 +1,39 @@
+package rankerEvaluation;
+
+import java.util.List;
+
+import ranker.algorithms.PerfectRanker;
+import ranker.algorithms.Ranker;
+import weka.classifiers.Classifier;
+import weka.core.Instance;
+import weka.core.Instances;
+
+public class Loss implements RankerEvaluationMeasure {
+
+	@Override
+	public double evaluate(Ranker ranker, Instances train, Instances test) {
+		// TODO Auto-generated method stub
+		try {
+			Ranker oracle = new PerfectRanker();
+			oracle.buildRanker(train);
+			ranker.buildRanker(train);
+			
+			for (Instance instance : test) {
+				List<Classifier> perfectRanking = oracle.predictRankingforInstance(instance);
+				List<Classifier> predictedRanking = ranker.predictRankingforInstance(instance);
+				
+				for (int i = 0; i < perfectRanking.size(); i++) {
+					if (perfectRanking.get(i).getClass().getName().equals(predictedRanking.get(0).getClass().getName())) {
+						
+						break;
+					}
+				}
+			}
+			
+		} catch (Exception e) {
+			// TODO log
+		}
+		return 0;
+	}
+
+}
