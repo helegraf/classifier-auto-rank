0f21b59b8f8332d67b06ff09a3ba87512fedd732
Helena Graf <Graf.Helena@web.de>
2018-01-22 10:45:01 +0100

    Add getPerformanceMeasures method to PerfectRanker

--- ranker/src/main/java/ranker/algorithms/PerfectRanker.java
+++ ranker/src/main/java/ranker/algorithms/PerfectRanker.java
@@ -1,7 +1,9 @@
 package ranker.algorithms;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import de.upb.cs.is.jpl.api.dataset.defaultdataset.relative.Ranking;
 import de.upb.cs.is.jpl.api.exception.algorithm.PredictionFailedException;
 import weka.classifiers.Classifier;
 import weka.core.Instance;
@@ -15,6 +17,8 @@ import weka.core.Instance;
  *
  */
 public class PerfectRanker extends PreferenceRanker {
+	
+	protected List<Double> performanceMeasures;
 
 	@Override
 	protected void initialize() throws Exception {
@@ -23,7 +27,27 @@ public class PerfectRanker extends PreferenceRanker {
 
 	@Override
 	public List<Classifier> predictRankingforInstance(Instance instance) throws PredictionFailedException {
-		return mapToOrdering(getRankingForInstance(instance));
+		Ranking ranking = getRankingForInstance(instance);
+		
+		// Get actual performance measures
+		performanceMeasures = new ArrayList<Double>();
+		for (int label : ranking.getObjectList()) {
+			int algorithmIndex = reverseIndicesMap.get(label);
+			performanceMeasures.add(instance.value(algorithmIndex));
+		}
+		
+		return mapToOrdering(ranking);
+	}
+
+	/**
+	 * Returns the actual performance measures for the last ranking that was
+	 * requested. The value at position i is the performance measure of the learning
+	 * algorithm at position i in the returned ranking.
+	 * 
+	 * @return The performance measures
+	 */
+	public List<Double> getPerformanceMeasuresForRanking() {
+		return performanceMeasures;
 	}
 
 }
