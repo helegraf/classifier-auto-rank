ccb0578e08a0e1ae2d437a840d08a426f2085366
Helena Graf <Graf.Helena@web.de>
2018-01-23 13:20:57 +0100

    Complete Loss evaluation class

--- ranker/src/main/java/rankerEvaluation/Loss.java
+++ ranker/src/main/java/rankerEvaluation/Loss.java
@@ -12,28 +12,47 @@ public class Loss implements RankerEvaluationMeasure {
 
 	@Override
 	public double evaluate(Ranker ranker, Instances train, Instances test, List<Integer> targetAttributes) {
-		// TODO Auto-generated method stub
+		int result = 0;
+		int numInstancesCalculated = test.numInstances();
+
 		try {
-			Ranker oracle = new PerfectRanker();
+			PerfectRanker oracle = new PerfectRanker();
 			oracle.buildRanker(train, targetAttributes);
 			ranker.buildRanker(train, targetAttributes);
-			
+
 			for (Instance instance : test) {
-				List<Classifier> perfectRanking = oracle.predictRankingforInstance(instance);
-				List<Classifier> predictedRanking = ranker.predictRankingforInstance(instance);
-				
-				for (int i = 0; i < perfectRanking.size(); i++) {
-					if (perfectRanking.get(0).getClass().getName().equals(predictedRanking.get(i).getClass().getName())) {
-						
-						break;
+				try {
+					List<Classifier> perfectRanking = oracle.predictRankingforInstance(instance);
+					List<Classifier> predictedRanking = ranker.predictRankingforInstance(instance);
+					
+					// Find the place at which the ranker has put the algorithm that in reality is
+					// the best
+					for (int i = 0; i < predictedRanking.size(); i++) {
+						if (perfectRanking.get(0).getClass().getName()
+								.equals(predictedRanking.get(i).getClass().getName())) {
+							// Compute difference
+							List<Double> performanceMeasures = oracle.getPerformanceMeasuresForRanking();
+							double loss = performanceMeasures.get(0) - performanceMeasures.get(i);
+							result += loss;
+							break;
+						}
 					}
+
+				} catch (Exception e) {
+					// TODO log
+					numInstancesCalculated--;
 				}
 			}
-			
+
 		} catch (Exception e) {
 			// TODO log
 		}
-		return 0;
-	}
 
+		// TODO maybe better solution than this (+ also for Kendall)
+		if (numInstancesCalculated != 0) {
+			result = result/numInstancesCalculated;
+		}
+		
+		return result;
+	}
 }
